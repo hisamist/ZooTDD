90f487d273e0c9d5f029059f874a06324c87f394
hisamist <hisamistolz@gmail.com>
2026-06-10 09:20:39 +0200

    test(REQ-Z-002): test rouge GetAnimalById

--- src/Zoo.UnitTests/ZooManagerTests.cs
+++ src/Zoo.UnitTests/ZooManagerTests.cs
@@ -25,5 +25,25 @@ public class ZooManagerTests
         assignedId.Should().Be(1);
     }
 
+    [Fact]
+    [Trait("Requirement", "REQ-Z-002")]
+    public void GetAnimal_ExistingId_ReturnsAnimal()
+    {
+        // Arrange
+        var zoo = new ZooManager();
+        var lion = new Animal
+        {
+            Id = 1,
+            Name = "Simba",
+            Category = AnimalCategory.Carnivore
+        };
+        zoo.AddAnimal(lion);
+
+        // Act
+        var result = zoo.GetAnimal(1);
+
+        // Assert
+        result.Should().BeEquivalentTo(lion);
+    }
 
 }
