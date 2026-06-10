1be9e64ed8c33a2a1fcd973713c453e6eb668bfd
hisamist <hisamistolz@gmail.com>
2026-06-10 09:18:45 +0200

    feat(REQ-Z-001): implémentation AddAnimal

--- src/Zoo.Domain/ZooManager.cs
+++ src/Zoo.Domain/ZooManager.cs
@@ -3,9 +3,13 @@ namespace Zoo.Domain;
 public class ZooManager
 {
     public const int MaxCapacity = 50;
+    private readonly Dictionary<int, Animal> _animals = new();
 
-    // À implémenter en TDD :
-    public int AddAnimal(Animal animal) => throw new NotImplementedException();
+    public int AddAnimal(Animal animal)
+    {
+        _animals[animal.Id] = animal;
+        return animal.Id;
+    }
     public Animal? GetAnimal(int id) => throw new NotImplementedException();
     public int TotalAnimals => throw new NotImplementedException();
     public int TotalCapacityUsed => throw new NotImplementedException();
